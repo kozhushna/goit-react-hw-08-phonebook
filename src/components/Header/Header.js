3cdbff6b74b2f9afd33192f51e8059f914a86924
kozhushna <k.kozhushna@gmail.com>
2023-05-25 12:06:52 +0300

    Add home page style

---
+++ src/components/Header/Header.js
@@ -0,0 +1,44 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+// import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Navigation } from '../Navigation/Navigation';
+import { UserMenu } from '../UserMenu/UserMenu';
+import { AuthNav } from '../AuthNav/AuthNav';
+import { useAuth } from '../../hooks/UseAuth';
+
+export const Header = () => {
+  const { isLoggedIn } = useAuth();
+
+  return (
+    <header>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <Navigation />
+            </Typography>
+            {isLoggedIn ? <UserMenu /> : <AuthNav />}
+            {/* <Button color="inherit">
+              
+            </Button> */}
+          </Toolbar>
+        </AppBar>
+      </Box>
+      {/* <Navigation />
+      {isLoggedIn ? <UserMenu /> : <AuthNav />} */}
+    </header>
+  );
+};
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,26 @@
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+
 export default function Home() {
   return (
-    <div>
-      <h1>Phonebook welcome page</h1>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="sm">
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <div>
+            <h1>Phonebook welcome page</h1>
+          </div>
+        </Box>
+      </Container>
+    </React.Fragment>
   );
 }
