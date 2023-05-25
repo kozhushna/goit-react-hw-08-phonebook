09bacc22d9e055d97978c54297ab1a32fe712367
kozhushna <k.kozhushna@gmail.com>
2023-05-25 14:27:28 +0300

    Update Contacts.js

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -5,7 +5,7 @@ import Container from '@mui/material/Container';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { ContactList } from 'components/ContactList';
-import ContactForm from 'components/ContactForm';
+import ContactForm from 'components/ContactForm/ContactForm';
 import Filter from 'components/Filter';
 import { fetchContacts } from '../redux/contacts/operations';
 import { selectIsLoading } from '../redux/contacts/selectors';
