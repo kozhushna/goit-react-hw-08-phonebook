d39d4b8f12c8ab8e4f09717d56b0891f3eba6b0a
kozhushna <k.kozhushna@gmail.com>
2023-05-25 14:48:52 +0300

    update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { store, persistor } from './redux/store';
-import App from './components/App/App';
+import App from './components/App';
 
 import './index.css';
 
